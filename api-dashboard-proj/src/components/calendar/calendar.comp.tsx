0d3ccc7cccf60dd2d4cca7db1360231d15f578ce
John Ramos <jramos5883@gmail.com>
2023-04-18 16:40:28 -0700

    Tinkering around with calendar apis

---
+++ api-dashboard-proj/src/components/calendar/calendar.comp.tsx
@@ -0,0 +1,11 @@
+import "./calendar.styles.css";
+
+const Calendar = () => {
+  return (
+    <div className='calandar-container'>
+      <h1>Calendar!</h1>
+    </div>
+  );
+};
+
+export default Calendar;
